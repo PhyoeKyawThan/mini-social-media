6b2a52612e14e58aaead163c11da3c6012948f5e
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2023-05-07 16:50:12 +0630

    some style change

--- social/static/js/script.js
+++ social/static/js/script.js
@@ -1,9 +1,13 @@
 let post_btn = document.getElementById('post-btn');
 let post_section = document.getElementById('post-section');
 let post_form = document.getElementById('post-form');
-
+let upload    = document.getElementById('upload-btn');
 
 post_btn.addEventListener('click', ()=>{
     post_form.style.display = 'block';
     post_btn.style.display  = 'none';
+})
+
+upload.addEventListener('click', ()=>{
+    post_form.style.display = 'none';
 })
